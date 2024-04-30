698667260d1f23ad095867f4b43788f4e3a9e41a
c6 <mlevin0210@gmail.com>
2024-04-30 18:14:48 +0300

    Update main.py

--- main.py
+++ main.py
@@ -10,20 +10,21 @@ def download_comic():
     comic_num = random.randint(1, Num_of_all_comics)
     filename = f'random_comic_num{comic_num}.png'
     url = f'https://xkcd.com/{comic_num}/info.0.json'
+    response_json = response.json()
     response = requests.get(url)
     response.raise_for_status()
-    image_url = response.json()['img']
+    image_url = response_json['img']
     image = requests.get(image_url)
     image.raise_for_status()
     with open(filename, 'wb') as file:
         file.write(image.content)
-    authors_comment = response.json()['alt']
+    authors_comment = response_json['alt']
     return authors_comment, filename
 
 
 # def get_groups_id():
 #     params = {
-#         'access_token': _ACCESS_TOKEN,
+#         'access_token': access_token,
 #         'v': '5.131', 
 #         'filter': 'admin'
 #     }
@@ -32,14 +33,15 @@ def download_comic():
 #     return group_id
 
 
-def get_servers_address():
+def get_servers_address(access_token):
     params = {
-        'access_token': _ACCESS_TOKEN,
+        'access_token': access_token,
         'v': '5.131', 
     }
     response = requests.get(f'https://api.vk.com/method/photos.getWallUploadServer', params=params)
-    response.raise_for_status
-    upload_url = response.json()['response']['upload_url']
+    response.raise_for_status()
+    response_json = response.json()
+    upload_url = response_json['response']['upload_url']
     return upload_url
 
 
@@ -49,31 +51,33 @@ def upload_comic_to_server(filename, server_address):
             'photo': file,
         }
         response = requests.post(server_address, files=files)
-    photo = response.json()['photo']
-    picture_hash = response.json()['hash']
-    server = response.json()['server']
-    response.raise_for_status
+    response.raise_for_status()
+    response_json = response.json()
+    photo = response_json['photo']
+    picture_hash = response_json['hash']
+    server = response_json['server']
     return server, photo, picture_hash
 
 
-def saveing_comic_in_group_album(server, photo, picture_hash):
+def save_comic_in_group_album(server, photo, picture_hash, access_token):
     files = {
-    'access_token': (_ACCESS_TOKEN),
+    'access_token': (access_token),
     'photo': (photo),
     'server': (server),
     'hash': (picture_hash),
     'v': ('5.131')
     }
     response = requests.post('https://api.vk.com/method/photos.saveWallPhoto', files=files)
-    owner_id = response.json()['response'][0]['owner_id']
-    photo_id = response.json()['response'][0]['id']
-    response.raise_for_status
+    response.raise_for_status()
+    response_json = response.json()
+    owner_id = response_json['response'][0]['owner_id']
+    photo_id = response_json['response'][0]['id']
     return owner_id, photo_id
 
 
-def publishing_comic_on_wall(authors_comment, filename, photo, owner_id, photo_id, group_id):
+def publish_comic_on_wall(authors_comment, filename, photo, owner_id, photo_id, group_id, access_token):
     files = {
-    'access_token': (_ACCESS_TOKEN),
+    'access_token': (access_token),
     'attachments': (f'photo{owner_id}_{photo_id}'),
     'message': (authors_comment),
     'v': ('5.131'),
@@ -85,17 +89,17 @@ def publishing_comic_on_wall(authors_comment, filename, photo, owner_id, photo_i
     response.raise_for_status()
 
 if __name__ == '__main__':
-    _ACCESS_TOKEN = os.environ['VK_ACCESS_TOKEN']
-    _GROP_ID = os.environ['VK_GROUP_ID']
     load_dotenv()
+    access_token = os.environ['VK_ACCESS_TOKEN']
+    group_id = os.environ['VK_GROUP_ID']
     Num_of_all_comics = 2842
     try:
         authors_comment, filename = download_comic()
         group_id = get_groups_id()
         client_id = os.environ['VK_CLIENT_ID']
-        server_address = get_servers_address()
+        server_address = get_servers_address(access_token)
         server, photo, picture_hash = upload_comic_to_server(filename, server_address)
-        owner_id, photo_id = saveing_comic_in_group_album(server, photo, picture_hash)
-        publishing_comic_on_wall(authors_comment, filename, photo, owner_id, photo_id, group_id)
+        owner_id, photo_id = save_comic_in_group_album(server, photo, picture_hash, access_token)
+        publish_comic_on_wall(authors_comment, filename, photo, owner_id, photo_id, group_id, access_token)
     finally:
         os.remove(filename)
